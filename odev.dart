9085981517e5ca72cd292cf006b8c74982513698
Saadet Gökmen <127449326+saadettt@users.noreply.github.com>
2024-05-20 17:47:20 +0300

    Odev3

---
+++ odev.dart
@@ -0,0 +1,24 @@
+void main(List<String> args) {
+  int age = 24;
+  String name = "Saadet";
+  bool resit = true;
+  dynamic dynamicVariable = 24;
+  var charVariable = 'C';
+  double doubleVariable = 24.0;
+
+  print(
+      "Yaş : $age  Adım: $name  Reşit mi: $resit  Dinamik Değişken: $dynamicVariable  Karakter Değişken: $charVariable  Double Değişken: $doubleVariable");
+
+  int myAge = 24;
+  String myName = "Saadet";
+  dynamic MyDynamicVariable = 24;
+
+  print("Yaşım: $myAge  Adım: $myName  Dinamik Değişken: $MyDynamicVariable");
+
+  String myName2 = "Saadet";
+  String mySurname = "Gökmen";
+  int myAge2 = 24;
+  bool isLegalAge = true;
+
+  print("Ben $myName2 $mySurname  Yaşım $myAge2  Reşitlik durumum $isLegalAge");
+}
